b7e5e2b58871e6c0ebe222d1c90944aa12a4dbeb
chenguang <chenguang0508@163.com>
2021-04-18 13:29:46 +0800

    更新代码

---
+++ flutter_module_fb/lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_boost/flutter_boost.dart';
+import 'package:flutter_module_fb/flutter_page_one.dart';
+import 'package:flutter_module_fb/flutter_page_two.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  MyApp({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+
+    //注册 native传递的打开flutter页面的key和页面的关系。
+    FlutterBoost.singleton.registerPageBuilders({
+      'first_boost': (pageName, params, _) => TwoPageWidget(
+            params: params,
+            message: pageName,
+          ),
+      'second_boost': (pageName, params, _) => TwoPageWidget(
+            params: params,
+            message: pageName,
+          ),
+      'tab_boost': (pageName, params, _) => TwoPageWidget(
+            params: params,
+            message: pageName,
+          ),
+      'flutterFragment_boost': (pageName, params, _) => TwoPageWidget(
+            params: params,
+            message: pageName,
+          ),
+
+      //可以在native层通过 getContainerParams 来传递参数
+      'flutter_one_page_boost': (pageName, params, _) {
+        print("flutter层：收到native参数params:$params");
+        return OnePageWidget(
+          params: params,
+          message: pageName,
+        );
+      },
+      'flutter_two_page_boost': (pageName, params, _) {
+        print("flutter层：收到native参数params:$params");
+        return TwoPageWidget(
+          params: params,
+          message: pageName,
+        );
+      },
+      'flutter_three_page_boost': (pageName, params, _) => TwoPageWidget(
+            params: params,
+            message: pageName,
+          ),
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      builder: FlutterBoost.init(postPush: _onRoutePushed),
+      home: Container(),
+    );
+  }
+
+  void _onRoutePushed(
+      String pageName, String uniqueId, Map params, Route route, Future _) {}
+}
