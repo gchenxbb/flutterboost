04e2972c254f235b018116840d2bb62cae2a8da8
chenguang <chenguang0508@163.com>
2021-04-19 22:58:16 +0800

    更新代码

---
+++ flutter_module_fb/.android/app/src/main/java/com/example/flutter_module_fb/host/MainActivity.java
@@ -0,0 +1,7 @@
+package com.example.flutter_module_fb.host;
+
+import android.os.Bundle;
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
